4119e2fa1ef9f0d6cdf57bba2a4c4af1eef00c71
aqifsariman <aqifsariman@gmail.com>
2022-04-27 21:16:36 +0800

    Deploying postgres

--- pool.js
+++ pool.js
@@ -1,14 +1,27 @@
 import pg from 'pg';
 
 const { Pool } = pg;
+let pgConnectionConfigs;
 
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+else {
 // set the way we will connect to the server
-const pgConnectionConfigs = {
-  user: 'aqifsariman',
-  host: 'localhost',
-  database: 'explore',
-  port: 5432,
-};
+  pgConnectionConfigs = {
+    user: 'aqifsariman',
+    host: 'localhost',
+    database: 'explore',
+    port: 5432,
+  };
+}
 
 const pool = new Pool(pgConnectionConfigs);
 
